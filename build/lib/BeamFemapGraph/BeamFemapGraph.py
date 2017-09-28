096a3b837b679432b8ab92912d551fb70d1fb075
Yuriy Vdovik <babah.yuriy06@gmail.com>
2017-09-28 09:03:24 +0400

    version-1.1

--- build/lib/BeamFemapGraph/BeamFemapGraph.py
+++ build/lib/BeamFemapGraph/BeamFemapGraph.py
@@ -1,29 +1,38 @@
 import re
 import matplotlib.pyplot as plt
+
+
 class FemapRsuBeam:
     def __init__(self, seysmik, wind_y, wind_x):
-        self.seysmik = seysmik
+        self.seysmik = seysmik  # для отладочных целей
         self.wind_y = wind_y
         self.wind_x = wind_x
-        SmFile = open(self.seysmik, "r")
-        self.mLinesSm = SmFile.readlines()
-        SmFile.close()
-        WyFile = open(self.wind_y, "r")
-        self.mLinesWy = WyFile.readlines()
-        WyFile.close()
-        WxFile = open(self.wind_x, "r")
-        self.mLinesWx = WxFile.readlines()
-        WxFile.close()
+
+        self.mLinesSm = self.openReadClose(seysmik)  # записали из файлов массивы строк
+        self.mLinesWy = self.openReadClose(wind_y)
+        self.mLinesWx = self.openReadClose(wind_x)
+
+        self.mnSm = []  # массивы усилий
+        self.mnWx = []
+        self.mnWy = []
+
+    @staticmethod
+    def openReadClose(name):
+        f = open(name, "r")
+        m = f.readlines()
+        f.close()
+        return m
+
     def numbers(self, mArg):
         pat = "(\d\.\d+E[-+]\d+)\s+"
-        self.mnSm = []; self.mnWx = []; self.mnWy = []
+
         for item in mArg:
             pass
-            searchPat = "\s+" + item + "\s+\d+\s+\d\s+" + pat + pat + pat + pat + pat + pat
+            searchPat = "\s+" + str(item) + "\s+\d+\s+\d\s+" + pat + pat + pat + pat + pat + pat
             fPat = re.compile(searchPat)
-            self.mnSm.append(self.eachFileN(self.mLinesSm, fPat))
-            self.mnWy.append(self.eachFileN(self.mLinesWy, fPat))
-            self.mnWx.append(self.eachFileN(self.mLinesWx, fPat))
+            self.mnSm.append(self.eachFileN(self.mLinesSm, fPat, item))
+            self.mnWy.append(self.eachFileN(self.mLinesWy, fPat, item))
+            self.mnWx.append(self.eachFileN(self.mLinesWx, fPat, item))
         self.plotMy(self.mnSm, 'seysmik_My')
         self.plotQz(self.mnSm, 'seysmik_Qz')
         self.plotMy(self.mnWy, "Wy_My")
@@ -33,29 +42,35 @@ class FemapRsuBeam:
 
         plt.show()
 
-    def eachFileN(self, LinesInFile, fPat):
+    def eachFileN(self, linesInFile, fPat, item):
         m = []
-        for i in LinesInFile:
+        for i in linesInFile:
             f = re.search(fPat, i)
-            if f != None:
+            if f is not None:  # срабатывает только раз, т.к в файле только один такой КЕ с таким fPat
                 n1 = f.group(1)
                 n2 = f.group(2)
                 n3 = f.group(3)
                 n4 = f.group(4)
                 n5 = f.group(5)
                 n6 = f.group(6)
-                m.append([n1,n2,n3,n4,n5,n6])
+                m.append([n1, n2, n3, n4, n5, n6])
+            if m == []:
+                print("\nнет такого КЕ -- ", item)
         return m
+
     def plotMy(self, m, title):
         self.plot(m, title, 4)
+
     def plotMz(self, m, title):
         self.plot(m, title, 5)
+
     def plotQz(self, m, title):
         self.plot(m, title, 2)
-    def plot(self, m, title, n):
+
+    def plot(self, m, title, n):  # n - это вид усилий
         i = 0
         plt.title(title)
         for item in m:
-            plt.plot([i, i + 1],[item[0][n], item[1][n]])
-            i=+1
+            plt.plot([i, i + 1], [item[0][n], item[1][n]])
+            i = i + 1
         plt.show()
--- example.py
+++ example.py
@@ -1,3 +1,3 @@
 from BeamFemapGraph.BeamFemapGraph import FemapRsuBeam as fm
 test = fm(seysmik="G:/temp/analiz/seysmik", wind_x="G:/temp/analiz/wind_x", wind_y="G:/temp/analiz/wind_y")
-test.numbers(["1908", "1907"])
+test.numbers([4074, 4075, 4076])
